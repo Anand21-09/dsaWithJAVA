873471caf110e7dfe8b8ee17d5c6b7c4411e026b
Anand21-09 <anandkumar001k@gmail.com>
2026-02-03 10:40:04 +0530

    Add files via upload

---
+++ String/myString.java
@@ -0,0 +1,35 @@
+
+public class myString {
+
+    public static void main(String[] args) {
+        //String str = "iroN MaN";
+        //System.out.println(str.toLowerCase());
+        //System.out.println(str.toUpperCase());
+        //System.out.println(str.charAt(3));
+        //System.out.println(str.indexOf('M'));
+        //System.out.println(str.lastIndexOf('N'));
+
+        // char ch[] = str.toCharArray();
+        // for (int i = 0; i < ch.length; i++) {
+        //     System.out.print(ch[i] + " ");
+        // }
+        // System.out.println("ANAND".compareTo("ANA"));
+        // System.out.println("ANAND".compareTo("ANAND"));
+        // System.out.println("ANA".compareTo("ANAND"));
+        // System.out.println("SHASH".compareTo("AMASH"));
+        // System.out.println("           ANAND           ".trim());
+        // System.out.println("ANAND KUMAR".startsWith("ANAND"));
+        // System.out.println("ironman".substring(3));
+        // String myVfxStudios = "RedChillies, Redefine, Yfx, NYVFXWALA";
+        // String vfxArray[] = myVfxStudios.split(",");
+        // for (int i = 0; i < vfxArray.length; i++) {
+        //     System.out.print(vfxArray[i] + " ");
+        // }
+        //String email = "anandkumar001k@gmail.com";
+        // int a = email.indexOf("@");
+        // int b = email.indexOf(".");
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(email.substring(a + 1, b));
+    }
+}
